0ff0d8925dbfd93735a899b0bebc6195f14c2bff
ufaz <talk2ufaz@gmail.com>
2022-11-29 09:37:22 -0800

    Added:9-last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     last_digit = 0
     if number == 0:
-        print(f"{last_digit") 
+        print(f"{last_digit}") 
     else:
         last_digit = number % 10
         print(f"{last_digit}")
