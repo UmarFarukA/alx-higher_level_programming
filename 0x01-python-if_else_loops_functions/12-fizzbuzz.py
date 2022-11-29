caea511f9d777d58bcc42f6b9add0340c4c4f1a8
ufaz <talk2ufaz@gmail.com>
2022-11-29 10:02:43 -0800

    Added:12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for k in range(1, 100):
+    for k in range(1, 101):
         if (k % 5 == 0) and (k % 3 == 0):
             print("FizzBuzz", end=" ")
         elif k % 3 == 0:
             print("Fizz", end=" ")
         elif k % 5 == 0:
             if k == 100:
-                print("Buzz\n")
+                print("Buzz")
             else:
                 print("Buzz", end=" ")
         else:
