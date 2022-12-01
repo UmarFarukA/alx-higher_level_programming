9fb08f5ab2d5359d1a77829c0e71068dc97ab99b
ufaz <talk2ufaz@gmail.com>
2022-12-01 02:44:01 -0800

    making them executed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
-result = 0
-for i in range(1, n):
-    result += int(sys.argv[i])
-print("{}".format(result))
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    result = 0
+    for i in range(1, n):
+        result += int(sys.argv[i])
+    print("{}".format(result))
