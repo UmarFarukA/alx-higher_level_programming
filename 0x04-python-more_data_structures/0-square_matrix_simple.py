7732d49c057adb82375ccdacd671f5173f21f5b1
ufaz <talk2ufaz@gmail.com>
2022-12-08 13:39:27 -0800

    change mode

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for j in range(len(matrix)):
-        for k in range(len(matrix[j])):
-            i = matrix[j][k]
-            matrix[j][k] = i * i
-    return (matrix)
+    return ([list(map(lambda x: x + x, j)), for j in matrix])
