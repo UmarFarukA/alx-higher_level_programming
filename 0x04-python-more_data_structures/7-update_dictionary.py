23b199d80d3f8ac32f07c6f2a58ee446d955a0d3
ufaz <talk2ufaz@gmail.com>
2022-12-09 02:39:11 -0800

    Added:7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    dict_keys = [k for k in a_dictionary.keys()]
+    if key in dict_keys:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
