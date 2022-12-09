26ffea35d217c2d49537c7f04717b28af222f10f
ufaz <talk2ufaz@gmail.com>
2022-12-09 03:25:46 -0800

    Added:10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    dic_vals = [v for v in a_dictionary.values()]
+    mx = dic_vals[0]
+    for j in range(1, len(dic_vals)):
+        if mx > dic_vals[j]:
+            mx = dic_vals[j]
+    for k in a_dictionary:
+        if a_dictionary[k] == mx:
+            return (a_dictionary[k])
