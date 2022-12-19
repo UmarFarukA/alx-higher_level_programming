e8cecf23672909456b8a43f6b190eea2cf832bc4
ufaz <talk2ufaz@gmail.com>
2022-12-19 08:20:05 -0800

    Added:3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        print("{} / {} = {}".format(a, b, result))
