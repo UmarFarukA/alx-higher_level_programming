074a4176a9f7b5699afaf4938ac02a5e42c803db
ufaz <talk2ufaz@gmail.com>
2022-12-20 03:36:10 -0800

    Added:docstring

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+
+
+"""Defines a class Square"""
+
+
 def Square:
+    """Represents a class square"""
+
     def __init__(self, size=0):
+        """Initialize a new sqaure
+            Args:
+                size: (int) optional & defaullt to 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
