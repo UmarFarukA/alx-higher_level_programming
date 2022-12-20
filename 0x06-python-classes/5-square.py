a70ae0cbe3d683f24f4fdc22a99477b68e03296f
ufaz <talk2ufaz@gmail.com>
2022-12-20 12:42:07 -0800

    Added:5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+"""Defines class Square"""
+
+
+class Square:
+    """Represent class square"""
+
+    def __init__(self, size=0):
+        """Initiallize a new square
+            Args:
+                size: (int) default to 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the sqaure area
+            Return:
+                square of size
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints square based on size"""
+        if self.__size == 0:
+            print("")
+        else:
+            for j in range(0, self.__size):
+                for i in range(0, self.__size):
+                    print("#", end="")
+                print("")
