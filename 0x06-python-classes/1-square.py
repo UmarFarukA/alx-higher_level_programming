ecd746deb30fe40e6cea7f3672bc1b38f4a6ec86
ufaz <talk2ufaz@gmail.com>
2022-12-20 03:26:54 -0800

    Add:docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+
+
+"""Defines a class Sqaure"""
+
+
 class Square:
+    """Represents a classsquare"""
+
     def __init__(self, size):
+        """Initialize new Square
+
+            Args:
+                size (int): The size of new square
+        """
         self.__size = size
