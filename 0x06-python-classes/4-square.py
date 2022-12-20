8c9447590089cdaafc22e3b7dffffc31321897ca
ufaz <talk2ufaz@gmail.com>
2022-12-20 06:18:06 -0800

    Added:4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+"""Defines class Square"""
+
+
+class Square:
+    """Represent class square"""
+
+    def __init__(self, size=0):
+        """Initiallize a new square
+            Args:
+                size: (int) default to 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+        """Retrieve the size property
+            Return: size (int)
+        """
+        return self.__size
+
+    def area(self):
+        """Calculate the sqaure area
+            Args: Nothing
+            Return:
+                square of size
+        """
+        return self.__size ** 2
