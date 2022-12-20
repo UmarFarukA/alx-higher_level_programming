0191b30f6bcdcfbf46cad434c2eb9259071b325a
ufaz <talk2ufaz@gmail.com>
2022-12-20 14:05:15 -0800

    Added:6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,7 +52,7 @@ class Square:
             Return:
                 square of size
         """
-        return self.__size ** 2
+        return self.__size * self_size 
 
     def my_print(self):
         """Prints square based on size"""
