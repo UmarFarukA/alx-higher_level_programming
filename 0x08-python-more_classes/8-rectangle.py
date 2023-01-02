ba41f1ed3302367773a8586045ae2249cb8c1c1f
ufaz <talk2ufaz@gmail.com>
2023-01-02 12:05:39 -0800

    add:staticmethod

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -72,9 +72,9 @@ class Rectangle:
             Biggest or equla
         """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rec_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise TypeError("rec_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
