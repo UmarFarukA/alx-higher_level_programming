595bb9053b8dbbc1fd819724aad16bca32efefbb
ufaz <talk2ufaz@gmail.com>
2023-01-09 09:09:11 -0800

    Added:3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function for checking instances"""
+
+
+def is_kind_of_class(obj, a_class):
+    """A function that checks instance of obj ofa a_class
+    Args:
+        obj: The object to check for an instance
+        a_class: a class to check of obj of.
+    Returns: True if obj is instance of a_class
+        ortherwise - False
+    """
+    if (isinstance(obj, a_class) or isinstance(obj, object)):
+        return True
+    return False
