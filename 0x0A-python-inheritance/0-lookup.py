e20743e5a68deabbef5e8e92f7c9ffa4da0e25d4
ufaz <talk2ufaz@gmail.com>
2023-01-09 08:32:23 -0800

    Initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """Returns the list of obj
+    Args:
+        obj: A class
+    """
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This project Teaches Inheritance in Python
