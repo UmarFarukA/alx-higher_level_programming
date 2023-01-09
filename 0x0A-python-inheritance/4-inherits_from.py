6f925b38785fb6c810302e34bf5ea971d02b3d8a
ufaz <talk2ufaz@gmail.com>
2023-01-09 12:03:19 -0800

    Added:4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that checks for instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """This function checks if obj is an instance of a class
+    Args:
+        obj: Object if is an instance of a class
+        a_class: The class to check
+    Returns: True if obj is an instance of a class
+        otherwise - False
+    """
+    if issubclass(obj, a_class):
+        return True
+    return False
