881ab37ab814708a0d68ad8d2a396d6d1efabc06
ufaz <talk2ufaz@gmail.com>
2023-01-09 08:51:54 -0800

    Added:obj_type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,6 @@ def is_same_class(obj, a_class):
     obj: The object to check if an instance of a class
     a_class: The class use
     """
-    if not isinstance(obj, a_class):
-        return False
-    return True
+    if type(obj) == a_class:
+        return True
+    return False
