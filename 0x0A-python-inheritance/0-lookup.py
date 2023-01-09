e45ab0e636674b00d99e220ef711aedbbc73297c
ufaz <talk2ufaz@gmail.com>
2023-01-09 08:56:28 -0800

    Added:files

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Defines a function that display attributes"""
+
+
 def lookup(obj):
     """Returns the list of obj
     Args:
         obj: A class
+    Returns:
+        Collection of attributes and Methods
     """
     return (dir(obj))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class checking function"""
+
+
 def is_same_class(obj, a_class):
     """Defines function for checking instance of a class
     Args:
-    obj: The object to check if an instance of a class
-    a_class: The class use
+        obj: The object to check if an instance of a class
+        a_class: The class use
+    Returns:
+        True if obj is of type class ortherwise False
     """
     if type(obj) == a_class:
         return True
