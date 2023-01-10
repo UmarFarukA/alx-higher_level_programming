7110565bfea5ebb058ad3b59b6babd246fecf5d6
ufaz <talk2ufaz@gmail.com>
2023-01-10 07:01:51 -0800

    Initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function for File operations"""
+
+
+def read_file(filename=""):
+    """Function for reading a file
+    Args:
+        filename (str): name of file to be read
+    """
+    with open(filename, encoding="utf-8") as fr:
+        print("{}".format(fr.read()))
