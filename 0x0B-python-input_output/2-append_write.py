7190bb762dd1b2e74453175e723d68c5c94041e4
ufaz <talk2ufaz@gmail.com>
2023-01-10 07:59:18 -0800

    initial commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -11,12 +11,4 @@ def append_write(filename="", text=""):
         Number of characters written to filename
     """
     with open(filename, "a", encoding="utf-8") as fw:
-        fw.write(text)
-    numChars = 0
-    words = ""
-    with open(filename, encoding="utf-8") as f:
-        words = f.read()
-    for word in words:
-        for c in word:
-            numChars += 1
-    return (numChars)
+        return fw.write(text)
