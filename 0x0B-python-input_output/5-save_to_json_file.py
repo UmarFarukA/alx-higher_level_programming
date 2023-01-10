2ec47c5246b9daaa9db230e756bf613def5bff6c
ufaz <talk2ufaz@gmail.com>
2023-01-10 08:18:22 -0800

    Initial commit

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
         filename: Name of file
     """
     with open(filename, "w", encoding="utf-8") as fw:
-        json.dumps(my_obj, fw)
+        json.dump(my_obj, fw)
