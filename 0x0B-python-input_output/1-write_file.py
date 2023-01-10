ac363c2dd1ac6392309e91e2f5f23e7617579c47
ufaz <talk2ufaz@gmail.com>
2023-01-10 07:49:41 -0800

    added:code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,16 @@ def write_file(filename="", text=""):
     Args:
         filename (str): Name of the file to write to
         text (str): Data for be written to filename
+    Returns:
+        Number of characters written to filename
     """
     with open(filename, "w", encoding="utf-8") as fw:
         fw.write(text)
+    numChars = 0
+    words = ""
+    with open(filename, encoding="utf-8") as f:
+        words = f.read()
+    for word in words:
+        for c in word:
+            numChars += 1
+    return (numChars)
