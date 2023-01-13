606fdcae7ba94603a47f12a395593312ed078c4b
UmarFarukA <talk2ufaz@gmail.com>
2023-01-13 01:18:10 -0800

    add:update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,13 +25,13 @@ class Rectangle(Base):
         """Return the Rectangle width"""
         return (self.__width)
 
-    @setter.width
+    @width.setter
     def width(self, value):
         """Sets new value fo rectangle width
         Args:
             value (int): Newrectangle width
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an Integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -42,13 +42,13 @@ class Rectangle(Base):
         """Get the value of height"""
         return (self.__height)
 
-    @setter.height
+    @width.setter
     def height(self, value):
         """Set new value for Rectangle height
         Args:
             height (int): New value for rectangle
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -59,7 +59,7 @@ class Rectangle(Base):
         """Get the value of x"""
         return (self.__x)
 
-    @setter.x
+    @x.setter
     def x(self, value):
         """Set new value for x"""
         if value <= 0:
@@ -71,7 +71,7 @@ class Rectangle(Base):
         """Get the value of y"""
         return (self.__y)
 
-    @setter.y
+    @y.setter
     def y(self, value):
         """set new value for y"""
         if value <= 0:
@@ -84,8 +84,12 @@ class Rectangle(Base):
 
     def display(self):
         """Display Rectangle instance with #"""
-        for j in range(self.__height):
-            for k in range(self.__width):
+        for y in range(self.y):
+            print("")
+        for j in range(self.height):
+            for x in range(self.x):
+                print(" ", end="")
+            for k in range(self.width):
                 print("#", end="")
             print("")
 
@@ -95,3 +99,44 @@ class Rectangle(Base):
         string += str(self.__x) + "/" + str(self.__y) + "-"
         string += str(self.__width) + "/" + str(sself.__height) + "]"
         return string
+
+    def update(self, *args, **kwargs):
+        """Represents an update function to REctangle
+        Args:
+            *args: A variable no key-word arguments
+            **kwargs: A variable key-word arguments
+        """
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                if args[i] == 0:
+                    self.id = args[i]
+                elif args[i] == None:
+                    self.__init__(self.width, self.height, self.x, self.y)
+                    self.width = args[i]
+                    self.height = args[i + 1]
+                    self.x = args[i + 2]
+                    self.y = args[i + 3]
+                    break
+                elif args[i] == 1:
+                    self.width = args[i]
+                elif args[i] == 2:
+                    self.height = args[i]
+                elif args[i] == 3:
+                    self.x = args[i]
+                elif args[i] == 4:
+                    self.y = args[i]
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        seld.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
