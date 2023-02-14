8d929241c634ade5761393f19c3fcda3ed8e7c6b
UmarFarukA <talk2ufaz@gmail.com>
2023-02-14 14:30:02 -0800

    Added:qoutes

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates a second_table and inserts multiple records
 CREATE TABLE IF NOT EXISTS second_table(`id` INT PRIMARY KEY, `name` VARCHAR(256) NOT NULL,`score` INT);
-INSERT INTO second_table(id, name, score) VALUES (1, 'John' 10);
-INSERT INTO second_table(id, name, score) VALUES(2, 'Alex', 3);
-INSERT INTO second_table(id, name, score) VALUES(3, 'Bob', 14);
-INSERT INTO second_table(id, name, score) VALUES(4, 'George', 8);
+INSERT INTO second_table(`id`, `name`, `score`) VALUES (1, 'John' 10);
+INSERT INTO second_table(`id`, `name`, `score`) VALUES(2, 'Alex', 3);
+INSERT INTO second_table(`id`, `name`, `score`) VALUES(3, 'Bob', 14);
+INSERT INTO second_table(`id`, `name`, `score`) VALUES(4, 'George', 8);
