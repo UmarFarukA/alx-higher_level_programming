e04a41f3530a89e6738c527d104929d9c4414bc5
UmarFarukA <talk2ufaz@gmail.com>
2023-02-14 14:53:26 -0800

    Added:102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Display Temparature in F
-SELECT city, AVG(value) As avg_temp FROM temperatures GROUP By city ORDER By avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) As avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP By city ORDER By avg_temp DESC LIMIT 3;
