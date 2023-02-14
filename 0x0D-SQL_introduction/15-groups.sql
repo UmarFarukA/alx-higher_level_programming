c54eca82526f0ec54ca1a5b4aa76a53cff1465d5
UmarFarukA <talk2ufaz@gmail.com>
2023-02-14 14:16:04 -0800

    Added:comma

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Display the number score that are same score
-SELECT score COUNT(*) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
+SELECT score, COUNT(*) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,7 @@
 -- Creates a second_table and inserts multiple records
 CREATE TABLE IF NOT EXISTS `second_table`(
-	`id` INT
-	`name` VARCHAR(256),
+	`id` INT PRIMARY KEY
+	`name` VARCHAR(256) NOT NULL,
 	`store` INT
 	);
-INSERT INTO 
-	second_table(id, name, store)
-VALUES
-	(1, 'John' 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(4, 'George', 8);
+INSERT INTO second_table(id, name, store) VALUES (1, 'John' 10), (2, 'Alex', 3),(3, 'Bob', 14), (4, 'George', 8);
