99a3919d3c7e9fb9f77e7cc4184b87f3e15bc48b
UmarFarukA <talk2ufaz@gmail.com>
2023-02-14 14:47:00 -0800

    Initial Commit

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Display Temparature in F
-SELECT city, AVG(value) As avg_temp FROM temparatures GROUP By city ORDER By avg_temp DESC;
+SELECT city, AVG(value) As avg_temp FROM temperatures GROUP By city ORDER By avg_temp DESC;
