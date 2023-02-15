ee49b1a5a8e4c036746dfcf9066335468cc02a7a
UmarFarukA <talk2ufaz@gmail.com>
2023-02-15 14:15:46 -0800

    added code on sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- Creats database hbtn_0d_usa anda table 'cities'
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-	`id` INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+	`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	`state_id` INT NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
-	FOREING KEY (state_id) REFERENCES states (state_id)
+	 FOREIGN KEY(state_id) 
+	 REFERENCES `hbtn_0d_usa`.`states`(`id`)
 	);
