fd3536789ab7bd4dcea2eaa7d6393e4be7307663
UmarFarukA <talk2ufaz@gmail.com>
2023-02-15 14:00:59 -0800

    Added:order by clause

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List all cities contained in hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = state.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = state.id ORDER BY cities.id;
