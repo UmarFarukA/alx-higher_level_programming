f776f4e7f3e472ebaa238a572d322ebbc262428f
UmarFarukA <talk2ufaz@gmail.com>
2023-02-15 14:51:26 -0800

    corrected syntax

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,11 @@
--- Create a database and a user
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED WITH 'user_0d_2_pwd';
+-- Create hbtn_0d_2 database and user_0d_2
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
