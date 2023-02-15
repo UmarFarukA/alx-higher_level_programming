d8cc6466cf9bc49e731eece83144f646db076ee8
UmarFarukA <talk2ufaz@gmail.com>
2023-02-15 14:31:13 -0800

    Correct syntax

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- List all privileges of MySQL user
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
