694a3e6194ac3054034ec000d57d0ee133604c21
UmarFarukA <talk2ufaz@gmail.com>
2023-02-16 13:12:34 -0800

    initial commit

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- List all shows contained in the database
-SELECT ts.title, tsg.genre_id FROM tv_shows AS ts INNER JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id ORDER BY ts.title, tsg.genre_id;
+SELECT ts.title, tsg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id ORDER BY ts.title, tsg.genre_id;
