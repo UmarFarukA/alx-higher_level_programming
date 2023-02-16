79b3a7aba891f0c75415e017313e0d128cfd03fe
UmarFarukA <talk2ufaz@gmail.com>
2023-02-16 13:25:04 -0800

    initial commit

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+-- List all shows contained in the database
+SELECT ts.title, tsg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id WHERE tsg.genre_id IS NULL ORDER BY ts.title, tsg.genre_id;
