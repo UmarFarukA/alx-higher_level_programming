ae0da90d39589d09f60896d5e9de12dfa3dda576
UmarFarukA <talk2ufaz@gmail.com>
2023-03-19 04:31:51 -0700

    Initial commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""Listing all states from hbtn_0e_0_usa Database"""
+    
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """Listing all states from hbtn_0e_0_usa database"""
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
