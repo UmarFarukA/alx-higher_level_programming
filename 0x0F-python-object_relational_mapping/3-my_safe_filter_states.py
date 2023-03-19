8f5034fa6f3486bdbf94c4f7e6bbc31c7378752d
UmarFarukA <talk2ufaz@gmail.com>
2023-03-19 06:21:19 -0700

    Added:docstrings

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-# List all states from the hbtn_0e_0_usa database that
-# search base on user input
+"""List states base on user input"""
 
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     [print(row) for row in cur.fetchall() if row[1] == sys.argv[4]]
+    cur.close()
+    db.close()
