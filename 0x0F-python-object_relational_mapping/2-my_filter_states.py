572e07585dfdb6f90bd48e1e62c5544c704f0d41
UmarFarukA <talk2ufaz@gmail.com>
2023-03-19 06:09:27 -0700

    Added:argv

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,9 +7,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.arg[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = '{}'"
-    .format(sys.argv[4])
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE BINARY\
+                name='{}'".format(sys.argv[4]))
     [print(row) for row in cur.fetchall()]
+    cur.close()
+    db.close()
