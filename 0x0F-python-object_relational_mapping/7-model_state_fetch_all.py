12d0424107966f2b7193af2b9b96724a0252d6cd
UmarFarukA <talk2ufaz@gmail.com>
2023-03-19 07:07:28 -0700

    added:docstrings

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# Using sqlalchmemy to list all states
+"""Using sqlalchmemy to list all states"""
 
 
 import sys
 from sqlalachemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
