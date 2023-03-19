f78fbecd8981dce335e4ed7ff9feb008523612e7
UmarFarukA <talk2ufaz@gmail.com>
2023-03-19 06:42:35 -0700

    added:docstring

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-# List all cities from the hbtn_0e_0_usa database that
+"""List all cities of a states"""
 
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM cities INNER JOIN states
-    ON cities.state_id = states.id ORDER BY cities.id"
+    query = "SELECT * FROM cities INNER JOIN states\
+             ON cities.state_id = states.id ORDER BY cities.id"
     cur.execute(query)
     [", ".join(print(row[2]) for row in cur.fetchall()
                if row[4] == sys.argv[4])]
+    cur.close()
+    db.close()
