f25c6b80da4a16b95f9a204c80e27dcaef058c58
UmarFarukA <talk2ufaz@gmail.com>
2023-03-19 12:48:20 -0700

    added:docstring

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,14 +9,12 @@ Base = declarative_base()
 
 class City(Base):
     """Represent the city class
-    __tablename__: Defines the table name
-    id: Table Id
-    name: represents name of city
-    state_id: A foreign key
+        __tablename__: Defines the table name
+        id: Table Id
+        name: represents name of city
+        state_id: A foreign key
     """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
