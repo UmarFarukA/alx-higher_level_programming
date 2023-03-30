4b0446438c2216d28271087c6f8c0a4bbcdeb674
UmarFarukA <talk2ufaz@gmail.com>
2023-03-30 14:06:58 -0700

    correct:flags

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a post request to a given url
-curl -sX POST -d "email=test@gmail.com&subject=I will always be there for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
