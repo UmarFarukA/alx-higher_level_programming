ee98f2baf198fe2c0a177e67a6a91241edf7224c
UmarFarukA <talk2ufaz@gmail.com>
2023-03-31 14:20:17 -0700

    initial commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,13 +2,14 @@
 """Scripts that takes and send URL & display response"""
 import sys
 import urllib.request
+import urllib.error
 
 
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as response:
-            content = response.read().decode("ascii")
+            content = response.read().decode("utf-8")
             print("{}".format(content))
-    except HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
