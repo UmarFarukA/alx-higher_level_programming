a43de91f39309a11fb3fe657544296ab4fd8f89a
UmarFarukA <talk2ufaz@gmail.com>
2023-03-31 14:10:15 -0700

    add:type

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     res = request.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(res)))
+    print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
