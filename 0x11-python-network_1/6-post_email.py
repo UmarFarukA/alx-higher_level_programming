f180ee9755fd5fd869b6079c65b5f0c72a91a1c1
UmarFarukA <talk2ufaz@gmail.com>
2023-03-31 13:57:15 -0700

    add:brace

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,7 +6,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = {"email": sys.argv[2])
+    data = {"email": sys.argv[2])}
 
     response = requests.post(url, data=data)
     print("{}".format(response.text))
