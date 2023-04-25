374e334c30f0d3a4ce47b7553ccc1de9700b8ed8
UmarFarukA <talk2ufaz@gmail.com>
2023-04-25 07:35:38 -0700

    initial commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`).on('response', function (response) {
+  console.log(response['title']);
+});
