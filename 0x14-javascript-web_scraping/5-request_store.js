31bc038addfdffc20668b5e247772f4efa32aeed
UmarFarukA <talk2ufaz@gmail.com>
2023-04-25 09:29:41 -0700

    add:writeStream

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-request.get(process.argv[2], function (err, response, body) {
-  fs.createWriteStream(process.argv[3]);
-});
+request.get(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
