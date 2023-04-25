bd246a6016b541da28eaf310930d5540605e2f91
UmarFarukA <talk2ufaz@gmail.com>
2023-04-25 12:56:59 -0700

    add:get method

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request.get(url, function (err, response, body) {
   if (!err) {
     const characters = JSON.parse(body).characters;
