6a7cd58c7574f946225bb98091bf6d36d16b9039
UmarFarukA <talk2ufaz@gmail.com>
2023-04-25 10:07:38 -0700

    remove:http-get

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'http https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request.get(url, function (err, response, data) {
+request(url, function (err, response, data) {
   const characters = JSON.parse(body).characters;
   characters.forEach((character) => {
     request(character, function (err, response, body) {
