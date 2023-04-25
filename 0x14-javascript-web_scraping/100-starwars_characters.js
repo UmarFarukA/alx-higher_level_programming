f00af0a19e385eb9f0bc59ae51ddd5a9eb8fd204
UmarFarukA <talk2ufaz@gmail.com>
2023-04-25 12:51:10 -0700

    add:get method

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (err, response, body) {
+request.get(url, function (err, response, body) {
   if (!err) {
     const characters = JSON.parse(body).characters;
     characters.forEach((character) => {
-      request(character, function (err, response, body) {
+      request.get(character, function (err, response, body) {
         if (!err) {
           console.log(JSON.parse(body).name);
         }
