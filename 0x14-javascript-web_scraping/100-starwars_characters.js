1358c10346c654dc6910192faaf640ce1c72b2c9
UmarFarukA <talk2ufaz@gmail.com>
2023-04-25 10:12:20 -0700

    correct:url

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (err, response, data) {
+request(url, function (err, response, body) {
   const characters = JSON.parse(body).characters;
   characters.forEach((character) => {
     request(character, function (err, response, body) {
